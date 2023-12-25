be164cc13e21214946d1410ce82119b4f214d7f4
Людмила <142536689+tangogenlu1@users.noreply.github.com>
2023-12-26 00:50:23 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,13 +1,30 @@
-        int[] data = new int[10];
-        }
-        for (int i = 0; i < 10; i++) {
-    public static void main(String[] args) {
-        }
-public class RandomDataGenerator {
 
-    }
-            System.out.println("Random Number: " + item);
+    data = generate_random_data()
 
-        for (int item : data) {
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+def main():
+    return data
+def generate_random_data():
+if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+    for item in data:
+    main()
+
 }
-            data[i] = (int) (Math.random() * 100) + 1;
+    }
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+
+    const data = generateRandomData();
+
+    const data = [];
+function generateRandomData() {
+}
+    return data;
+function main() {
+    data.forEach(item => {
+    });
+main();
