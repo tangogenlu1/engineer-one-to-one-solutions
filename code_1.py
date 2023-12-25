21b3b0a9e3d846392a5e1f291fe5b9d55c048ea6
Людмила <142536689+tangogenlu1@users.noreply.github.com>
2023-12-25 19:47:59 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,13 +1,69 @@
+
+        for (int item : data) {
+    }
         for (int i = 0; i < 10; i++) {
+        }
+        int[] data = new int[10];
+public class RandomDataGenerator {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+
+            System.out.println("Random Number: " + item);
+}
+    public static void main(String[] args) {    }
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    const data = [];
+main();
+        console.log("Random Number: " + item);
+function main() {
+    data.forEach(item => {
+    return data;
+}
+function generateRandomData() {
+    });
+    const data = generateRandomData();
+
+}
+
+    for (let i = 0; i < 10; i++) {        }
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+    }
+    public static void main(String[] args) {
+        int[] data = new int[10];
 
             data[i] = (int) (Math.random() * 100) + 1;
         }
-public class RandomDataGenerator {
-        for (int item : data) {
-
-        int[] data = new int[10];
-    }
-}
-        }
-    public static void main(String[] args) {
             System.out.println("Random Number: " + item);
+public class RandomDataGenerator {
+
+}            System.out.println("Random Number: " + item);
+        }
+}
+    public static void main(String[] args) {
+        int[] data = new int[10];
+
+
+        for (int item : data) {
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+        }}
+main();
+
+        console.log("Random Number: " + item);
+    }
+    data.forEach(item => {
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    const data = generateRandomData();
+    });
+function generateRandomData() {
+    return data;
+function main() {
+    const data = [];
+
+    for (let i = 0; i < 10; i++) {
