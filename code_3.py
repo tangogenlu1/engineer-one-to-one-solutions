6c1ffc48df3a381235e3c2472e9890e275390143
Людмила <142536689+tangogenlu1@users.noreply.github.com>
2023-12-26 15:57:36 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,30 +1,65 @@
-
-    data = generate_random_data()
+def main():
+        print(f"Random Number: {item}")
 
     data = [random.randint(1, 100) for _ in range(10)]
-import random
-def main():
+if __name__ == "__main__":
+    main()
     return data
+
+
 def generate_random_data():
+import random
+
+    data = generate_random_data()
+    for item in data:def generate_random_data():
+
+    data = generate_random_data()
 if __name__ == "__main__":
 
-        print(f"Random Number: {item}")
-    for item in data:
+import random
+def main():
     main()
 
-}
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+    return data
+
+    for item in data:
+    public static void main(String[] args) {
+        }
     }
-        console.log("Random Number: " + item);
-        data.push(Math.floor(Math.random() * 100) + 1);
-    for (let i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
 
-    const data = generateRandomData();
-
-    const data = [];
-function generateRandomData() {
 }
-    return data;
-function main() {
-    data.forEach(item => {
-    });
-main();
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+        }
+        int[] data = new int[10];
+        for (int item : data) {
+    for item in data:
+    data = generate_random_data()
+    main()
+        print(f"Random Number: {item}")
+import random
+def main():
+
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+    return data
+def generate_random_data():    data = generate_random_data()
+    main()
+
+def main():
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    for item in data:
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    return data
+
+import random
