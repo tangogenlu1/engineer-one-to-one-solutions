4eab0b9e92a416018123a0bc0f6f315d8001274e
Людмила <142536689+tangogenlu1@users.noreply.github.com>
2023-12-26 05:52:45 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,69 +1,14 @@
 
-        for (int item : data) {
-    }
-        for (int i = 0; i < 10; i++) {
-        }
-        int[] data = new int[10];
-public class RandomDataGenerator {
-        }
-            data[i] = (int) (Math.random() * 100) + 1;
 
-            System.out.println("Random Number: " + item);
-}
-    public static void main(String[] args) {    }
-        data.push(Math.floor(Math.random() * 100) + 1);
+if __name__ == "__main__":
+import random
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+    for item in data:
+def generate_random_data():
 
-    const data = [];
-main();
-        console.log("Random Number: " + item);
-function main() {
-    data.forEach(item => {
-    return data;
-}
-function generateRandomData() {
-    });
-    const data = generateRandomData();
+    return data
+    main()
 
-}
-
-    for (let i = 0; i < 10; i++) {        }
-        for (int i = 0; i < 10; i++) {
-        for (int item : data) {
-    }
-    public static void main(String[] args) {
-        int[] data = new int[10];
-
-            data[i] = (int) (Math.random() * 100) + 1;
-        }
-            System.out.println("Random Number: " + item);
-public class RandomDataGenerator {
-
-}            System.out.println("Random Number: " + item);
-        }
-}
-    public static void main(String[] args) {
-        int[] data = new int[10];
-
-
-        for (int item : data) {
-        for (int i = 0; i < 10; i++) {
-public class RandomDataGenerator {
-    }
-            data[i] = (int) (Math.random() * 100) + 1;
-        }}
-main();
-
-        console.log("Random Number: " + item);
-    }
-    data.forEach(item => {
-}
-        data.push(Math.floor(Math.random() * 100) + 1);
-
-    const data = generateRandomData();
-    });
-function generateRandomData() {
-    return data;
-function main() {
-    const data = [];
-
-    for (let i = 0; i < 10; i++) {
+def main():
