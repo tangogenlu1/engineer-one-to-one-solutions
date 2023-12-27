287e97e605c6df29a67d2bbe79a9c37b6e52a69c
Людмила <142536689+tangogenlu1@users.noreply.github.com>
2023-12-27 07:04:53 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,14 +1,16 @@
 
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+function generateRandomData() {
+}
+    data.forEach(item => {
+    const data = [];
+}
+    const data = generateRandomData();
 
-if __name__ == "__main__":
-import random
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
-    for item in data:
-def generate_random_data():
-
-    return data
-    main()
-
-def main():
+    return data;
+    }
+    for (let i = 0; i < 10; i++) {
+function main() {
+    });
+        console.log("Random Number: " + item);
