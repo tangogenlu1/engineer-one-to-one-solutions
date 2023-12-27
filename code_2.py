ecab480cd7e2ece48a76b6c15b5069dc1810f96b
Людмила <142536689+tangogenlu1@users.noreply.github.com>
2023-12-27 17:09:43 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,26 +1,71 @@
-public class RandomDataGenerator {
+function main() {
+main();
 
-        }
-    public static void main(String[] args) {
-        }
+    return data;
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
+    });
+
     }
-        for (int i = 0; i < 10; i++) {
-        for (int item : data) {
-            data[i] = (int) (Math.random() * 100) + 1;
-
-        int[] data = new int[10];
+    const data = [];
+        console.log("Random Number: " + item);
 }
-            System.out.println("Random Number: " + item);    data = [random.randint(1, 100) for _ in range(10)]
-
+}
+    data.forEach(item => {
+        print(f"Random Number: {item}")
+def generate_random_data():
+def main():
+    return data
 
     data = generate_random_data()
-    return data
-        print(f"Random Number: {item}")
+    main()
 
 import random
-if __name__ == "__main__":
-    main()
     for item in data:
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+}
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+main();
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
 
+    const data = generateRandomData();
+    data.forEach(item => {
+
+    });
+    const data = [];
+
+function main() {
+}
+    return data;def generate_random_data():
+
+    return data
+        print(f"Random Number: {item}")
 def main():
-def generate_random_data():
+
+    main()
+
+    for item in data:
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+import random
+            data[i] = (int) (Math.random() * 100) + 1;
+
+    }
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+        }
+            System.out.println("Random Number: " + item);
+        int[] data = new int[10];
+        for (int item : data) {
+    public static void main(String[] args) {
+}
+
+        }
