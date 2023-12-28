e702343ec62f8f4ed271417f8e199c27f9e99aa6
Людмила <142536689+tangogenlu1@users.noreply.github.com>
2023-12-28 03:14:32 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,27 +1,65 @@
+        int[] data = new int[10];
+        }
+        }
+public class RandomDataGenerator {
 
-    for item in data:
-import random
-def main():
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
-def generate_random_data():
-        print(f"Random Number: {item}")
+}
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
 
-
-    return data
+        for (int item : data) {        print(f"Random Number: {item}")
 if __name__ == "__main__":
-if __name__ == "__main__":
-import random
-    data = generate_random_data()
-        print(f"Random Number: {item}")
-def main():
-
-    main()
-def generate_random_data():
-
     for item in data:
 
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+import random
+    main()
+def generate_random_data():
+    data = generate_random_data()
+
+
+    for item in data:
+if __name__ == "__main__":
+def main():
     return data
 
+
+        print(f"Random Number: {item}")
     data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+    main()
+import random
+def generate_random_data():
+
+    main()
+
+    for item in data:
+    data = generate_random_data()
+def main():
+import random
+
+        print(f"Random Number: {item}")
+    return data
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+    return data
+    main()
+if __name__ == "__main__":
+
+
+def main():
+
+import random
+    data = generate_random_data()
+        print(f"Random Number: {item}")
