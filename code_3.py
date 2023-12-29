6dab58fdf417ae673902e82d69c0d988d570a87f
Людмила <142536689+tangogenlu1@users.noreply.github.com>
2023-12-29 19:35:25 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,25 +1,64 @@
-
-            System.out.println("Random Number: " + item);
-}
         int[] data = new int[10];
-        for (int item : data) {
-        }
-            data[i] = (int) (Math.random() * 100) + 1;
-
-public class RandomDataGenerator {
-    public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+public class RandomDataGenerator {
     }
-        }    data = generate_random_data()
+        for (int item : data) {
+
+        }
+        }
+            System.out.println("Random Number: " + item);
+    return data
+    data = generate_random_data()
 
     for item in data:
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
 
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+    main()
+def main():
+if __name__ == "__main__":
         print(f"Random Number: {item}")
 
-def main():
-import random
-def generate_random_data():
+if __name__ == "__main__":
     main()
+    return data
+    for item in data:
+
+        print(f"Random Number: {item}")
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+
+import random
+def generate_random_data():        print(f"Random Number: {item}")
+def main():
+
+
+
+    main()
+import random
+    data = generate_random_data()
+    return data
+def generate_random_data():
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+    main()
+
+import random
+
+        print(f"Random Number: {item}")
+def generate_random_data():
+    for item in data:
+    return data
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+if __name__ == "__main__":
+
